d691ddaf8a9e734a17046cd6528ec69bf51e1499
Daniel Ault <dault@memphis.edu>
2013-10-09 17:49:32 -0500

    organized test code in class HW4

--- src/hw4/HW4.java
+++ src/hw4/HW4.java
@@ -14,6 +14,9 @@ public class HW4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Testing the BSTSet class....\n");
+        testSet();
+        System.out.println("\n\n\nTesting the HashMapChained rehash....\n");
         testHashMapChainedRehash();
     }
     
@@ -78,23 +81,23 @@ public class HW4 {
 	tmnt.add(18, "Krang");
 	tmnt.add(18, "Krang2");
 	System.out.println(tmnt);
-	//System.out.println(tmnt.get(18));
-	//tmnt.remove(18);
+	System.out.println(tmnt.get(18));
+	tmnt.remove(18);
 	System.out.println(tmnt);
-	//System.out.println(tmnt.get(18));
-	//tmnt.remove(90);
-	//tmnt.remove(29);
+	System.out.println(tmnt.get(18));
+	tmnt.remove(90);
+	tmnt.remove(29);
 	tmnt.add(23, "Bebop");
 	tmnt.add(73, "Technodrome");
 	tmnt.add(29, "Foot Soldier");
 	tmnt.add(32, "Baxter Stockman");
 		
 	System.out.println(tmnt);
-	/*	
+		
 	System.out.println(tmnt.get(13));
 	System.out.println(tmnt.get(18));
 	System.out.println(tmnt.get(23));
 	System.out.println(tmnt.get(24));
-        * */
+        
     }
 }
